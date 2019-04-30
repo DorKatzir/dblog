65302049aa005c081f984e23fa6246e1563c8539
DorKatzir <DorKatzir@users.noreply.github.com>
2019-04-30 10:42:26 +0300

    removes al css styles

--- tpl/header.php
+++ tpl/header.php
@@ -4,7 +4,7 @@
         <title><?= $page_title; ?></title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="css/mainStyle.css" rel="stylesheet" type="text/css"/>
+        <link href="css/main.css" rel="stylesheet" type="text/css"/>
     </head>
 <body class="wrapper">
     <header>
