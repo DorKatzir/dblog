8b9eb357892c7ffcb40128b35430dc80a2bca4d8
DorKatzir <DorKatzir@users.noreply.github.com>
2019-05-02 10:37:35 +0300

    homepage steps css styling and created js links functions

--- index.php
+++ index.php
@@ -9,15 +9,13 @@ $page_title = "Home";
     <main>
         <div class="content">
             <h1>Wellcome to dBlog</h1><br>
-           
+           <p>Follow these Steps:</p>
             <div class="stages">
-                <div class="infobox"><span>1</span><br> Create <br> your Account</div>
-                <div class="infobox"><span>2</span><br> Sign In <br> to your Account</div>
-                <div class="infobox"><span>3</span><br> Start <br> Blogging!</div>
+                <div class="infobox" onclick="info1()"><span>1</span><br> Create <br> Account</div>
+                <div class="infobox" onclick="info2()"><span>2</span><br> Sign In <br> with Account</div>
+                <div class="infobox" onclick="info3()"><span>3</span><br> Start <br> dBlogging!</div>
             </div>
        
-
-
 
         </div>
     </main>
--- js/jq.js
+++ js/jq.js
@@ -1,2 +1,15 @@
 $('div.sm-box').delay(3000).slideUp();
 
+
+
+function info1() {
+    window.location = 'signup.php';
+}
+
+function info2() {
+    window.location = 'signin.php';
+}
+
+function info3() {
+    window.location = 'blog.php';
+}
