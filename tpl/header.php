4d83e93029ca06b0a3a3dcedca5b34d17e927596
DorKatzir <DorKatzir@users.noreply.github.com>
2019-05-05 15:50:52 +0300

    added favicon

--- tpl/header.php
+++ tpl/header.php
@@ -4,6 +4,7 @@
         <title><?= $page_title; ?></title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="icon" href="./img/db_favicon.ico"> 
         <link href="css/main.css" rel="stylesheet" type="text/css"/>
     </head>
 <body class="multi-bg">
