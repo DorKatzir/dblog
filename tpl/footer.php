842aecd862ece21d27a0c44d7d1640b80cff1106
DorKatzir <DorKatzir@users.noreply.github.com>
2019-05-06 17:11:00 +0300

    footer creadits and restyled bg color and links

--- tpl/footer.php
+++ tpl/footer.php
@@ -1,15 +1,15 @@
         <footer>
             <h5>
                 <span>
-                &copy <?= date('Y'); ?>&nbsp;dBlog -
+                &copy <?= date('Y'); ?>&nbsp;/ Created by <a href="https://dor.digital" target="_blank">Dor katzir</a> /
                 </span>
 
                <span>
-                Special thanks to: Shlomi Lahav from <a href="https://www.hackeru.co.il/" target="_blank">"HackerU" College</a> -
+                Special thanks to Shlomi Lahav <a href="https://www.hackeru.co.il/" target="_blank">"HackerU" College</a> /
                </span>
                
                <span>
-                Image: wallpaperswide.com 
+                Image: <a href="http://wallpaperswide.com" target="_blank">wallpaperswide</a>
                </span>
             </h5>
         </footer>
